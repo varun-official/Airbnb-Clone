1021fc316ca5b1641a6e710a21bb4574ca5adba8
varun115 <50090823+varun115@users.noreply.github.com>
2020-09-24 18:08:47 +0530

    1st commit

---
+++ src/Header.js
@@ -0,0 +1,36 @@
+/** @format */
+
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
+function Header() {
+  const histroy = useHistory();
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header__icon"
+          src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+        />
+      </Link>
+      <div className="header__center">
+        <input onKeyPressCapture={() => histroy.push("/search")} type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
